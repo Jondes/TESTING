17c40b6bb67f666ea9e98ec87bfaa20b390a152d
Jondes <jondes560@hotmail.com>
2018-08-01 12:14:34 +0100

    added test and hook.spec

---
+++ jasmine-project/spec/support/hook.spec.js
@@ -0,0 +1,33 @@
+describe('describe01', function(){
+    
+beforeAll(function()){
+    console.log('running the beforeAll hook');
+});
+
+beforeEach(function()){
+    console.log('running the beforeEach hook');
+});
+
+afterEach(function()){
+    console.log('running the afterEach hook');
+});
+
+afterAll(function()){
+    console.log('running the afterAll hook');
+});
+    
+it('test01', function(){
+    console.log('running test 01');
+});
+    
+it('test01', function(){
+    console.log('running test 02');
+});
+    
+it('test01', function(){
+    console.log('running test 03');
+});
+    
+    
+    
+});
---
+++ jasmine-project/spec/support/test.spec.js
@@ -0,0 +1,22 @@
+/*describe('hello world'), function(){
+it('hello test', function(){
+
+});
+});*/
+
+describe('calculate addition', function(){
+    it('should be able to add two numbers'), function(){
+      console.log(' I was able to add two numbers');
+    });
+    it('should be able to add three numbers'), function(){
+      console.log(' I was able to add three numbers');
+    });
+    
+describe('calculate addition with minus numbers', function(){
+    it('should be able to add -2 and -2'), function(){
+      console.log(' I was able to add two numbers');
+    });
+    
+});
+        
+});
