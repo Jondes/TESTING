fbdcb315759bde599c4c046fc9c9cd55ebe97f7d
Jondes <jondes560@hotmail.com>
2018-08-02 22:11:53 +0100

    added test.spec and hook.spec

--- jasmine-project/spec/support/hook.spec.js
+++ jasmine-project/spec/support/hook.spec.js
@@ -1,30 +1,30 @@
-describe('describe01', function(){
-    
-beforeAll(function()){
-    console.log('running the beforeAll hook');
-});
+describe('describe01', function () {
 
-beforeEach(function()){
+    beforeAll(function ()){
+        console.log('running the beforeAll hook');
+    });
+
+beforeEach(function ()){
     console.log('running the beforeEach hook');
 });
 
-afterEach(function()){
+afterEach(function ()){
     console.log('running the afterEach hook');
 });
 
-afterAll(function()){
+afterAll(function ()){
     console.log('running the afterAll hook');
 });
-    
-it('test01', function(){
+
+it('test01', function () {
     console.log('running test 01');
 });
-    
-it('test01', function(){
+
+it('test01', function () {
     console.log('running test 02');
 });
-    
-it('test01', function(){
+
+it('test01', function () {
     console.log('running test 03');
 });
     
--- jasmine-project/spec/support/test.spec.js
+++ jasmine-project/spec/support/test.spec.js
@@ -4,18 +4,18 @@ it('hello test', function(){
 });
 });*/
 
-describe('calculate addition', function(){
-    it('should be able to add two numbers'), function(){
-      console.log(' I was able to add two numbers');
-    });
-    it('should be able to add three numbers'), function(){
-      console.log(' I was able to add three numbers');
-    });
-    
-describe('calculate addition with minus numbers', function(){
-    it('should be able to add -2 and -2'), function(){
-      console.log(' I was able to add two numbers');
-    });
+describe('calculate addition', function () {
+  it('should be able to add two numbers'), function () {
+    console.log(' I was able to add two numbers');
+  });
+it('should be able to add three numbers'), function () {
+  console.log(' I was able to add three numbers');
+});
+
+describe('calculate addition with minus numbers', function () {
+  it('should be able to add -2 and -2'), function () {
+    console.log(' I was able to add two numbers');
+  });
     
 });
         
