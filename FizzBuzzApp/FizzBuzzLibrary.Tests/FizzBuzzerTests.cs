02041ef3b3e51383a9dd92a94d708600ee4af561
johnson guedes <jondes560@hotmail.com>
2020-10-05 22:01:09 +0100

    Initial Commit
    Bank Testing

---
+++ FizzBuzzApp/FizzBuzzLibrary.Tests/FizzBuzzerTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace FizzBuzzLibrary.Tests
+{
+    [TestFixture]
+    public class FizzBuzzerTests
+    {
+        [Test] 
+        public void Buzzer_When1_Returns1()
+        {
+            //Arrange
+            int input = 1;
+            //Act
+            string output = FizzBuzzer.GetValue(input);
+
+            //Assert
+            Assert.AreEqual("1", output);
+        }
+        
+    }
+    }
---
+++ FizzBuzzApp/FizzBuzzLibrary/FizzBuzzer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzLibrary
+{
+    public class FizzBuzzer
+    {
+        public static string GetValue(int input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
