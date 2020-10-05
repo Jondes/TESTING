b04acf57135a705bf42d1074ff6af54c8545a230
johnson guedes <jondes560@hotmail.com>
2020-10-06 00:03:15 +0100

    Changes made
    Stick to original

--- FizzBuzzApp/FizzBuzz/Program.cs
+++ FizzBuzzApp/FizzBuzz/Program.cs
@@ -8,27 +8,30 @@ using System.Threading.Tasks;
 namespace FizzBuzz
 {
     class Program
-    {   //Print numbers from 1 to 100
+    {   //Print numbers from 1 to 50
         static void Main(string[] args)
         { 
-            for (int i = 1; i <= 100; i++)
-            {
-                string result = "";
-            //Divisible by 3 print Fizz
-              if(i % 3 == 0) result = "Fizz";
-            //Divisible by 5 print Buzz
-              if(i % 5 == 0) result = result + "Buzz";
-             //Divisible by 3 and 5 print FizzBuzz
-              if(i % 3 == 0 || i % 5 == 0) Console.WriteLine("FizzBuzz");
-
-            //Print the number as it is if not divisible by 3 or 5
-            if(result.Length== 0) result = i.ToString();
-
-            Console.WriteLine(result);
+            for (int i = 1; i <= 100; i++)  
+{  
+        if (i % 3 == 0 && i % 5 == 0)  
+        {  
+            Console.WriteLine("FizzBuzz");  
+        }  
+        else if (i % 3 == 0)  
+        {  
+           Console.WriteLine("Fizz");  
+        }  
+        else if (i % 5 == 0)  
+        {  
+           Console.WriteLine("Buzz");  
+        }  
+        else  
+        {  
+            Console.WriteLine(i);  
+        }  
+}  
             }
-           
-        }
+          }
     } 
     
-}
 
