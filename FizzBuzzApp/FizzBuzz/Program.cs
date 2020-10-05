028947858d81a781611d10fd3106fa211d2547d9
johnson guedes <jondes560@hotmail.com>
2020-10-05 22:48:56 +0100

    FizzBuzz Unique
    Diffrently done

--- FizzBuzzApp/FizzBuzz/Program.cs
+++ FizzBuzzApp/FizzBuzz/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,14 +8,25 @@ using System.Threading.Tasks;
 namespace FizzBuzz
 {
     class Program
-    {
+    {   //Print numbers from 1 to 100
         static void Main(string[] args)
-        {
-            for (int i = 1; i <= 10; i++)
+        { 
+            for (int i = 1; i <= 100; i++)
             {
-                Console.WriteLine(i);
+                string result = "";
+            //Divisible by 3 print Fizz
+              if(i % 3 == 0) result = "Fizz";
+            //Divisible by 5 print Buzz
+              if(i % 5 == 0) result = result + "Buzz";
+             //Divisible by 3 and 5 print FizzBuzz
+              if(i % 3 == 0 || i % 5 == 0) Console.WriteLine("FizzBuzz");
+
+            //Print the number as it is if not divisible by 3 or 5
+            if(result.Length== 0) result = i.ToString();
+
+            Console.WriteLine(result);
             }
-            Console.ReadLine();
+           
         }
     } 
     
